911a01c6294e16aeb95f466a7e4cd96a557c40b8
Jesus Antonio Gonzalez <114256805+954gonzalez@users.noreply.github.com>
2023-09-07 14:55:39 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
 
         if (this.form.valid) {
 
-          this.client.postRequest('https://back-despliegue.onrender.com/api/v01/user/login', {
+          this.client.postRequest('https://despliegue-back2.onrender.com/login', {
             email: this.form.value.email,
             password: this.form.value.password
           }).subscribe(
